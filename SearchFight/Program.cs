f2cad94c7168da28904cf7c2fae79f4502325eaf
Urdarum <Roberto.E.N.T@Gmail.com>
2017-09-18 19:59:43 -0500

    Add files via upload

---
+++ SearchFight/Program.cs
@@ -0,0 +1,79 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SearchFight
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            SearchBase Base = new SearchBase();
+
+            Console.Write("Please insert the number of terms to fight: ");
+            int Num = Convert.ToInt16(Console.ReadLine());
+            string[] Fighter = new string[Num];
+            long[] Gresults = new long[Num];
+            long[] Bresults = new long[Num];
+            long[] Tresults = new long[Num];
+
+            string GR;
+            string BR;
+
+            long temp;
+
+            int Gwinner = 0;
+            long GTop = 0;
+
+            int Bwinner = 0;
+            long BTop = 0;
+
+            int Twinner = 0;
+            long TTop = 0;
+
+            for(int i = 0; i < Num; i++)
+            {
+                Console.Write("Please insert the first item to fight: ");
+                Fighter[i] = Console.ReadLine();
+            }
+
+            for (int i=0; i < Num; i++)
+            {
+                GR = Base.GoogleQuery(Fighter[i]);
+                Gresults[i] = Convert.ToInt64(GR);
+                
+                if (GTop<Gresults[i])
+                {
+                    GTop = Gresults[i];
+                    Gwinner = i;
+                }
+
+                BR = Base.BingQuery(Fighter[i]);
+                Bresults[i] = Convert.ToInt64(BR);
+
+                if (BTop < Bresults[i])
+                {
+                    BTop = Bresults[i];
+                    Bwinner = i;
+                }
+
+                temp = Gresults[i] + Bresults[i];
+
+                if (TTop<temp)
+                {
+                    TTop = temp;
+                    Twinner = i;
+                }
+
+                Console.WriteLine(Fighter[i] + ": Google: " + Gresults[i] + " Bing: " +Bresults[i]);
+            }
+
+            Console.WriteLine("Google winner is : " + Fighter[Gwinner] + " With :" + GTop);
+            Console.WriteLine("Bing winner is : " + Fighter[Bwinner] + " With :" + BTop);
+            Console.WriteLine("Total winner is : " + Fighter[Twinner] + " With :" + TTop);
+            Console.ReadLine();
+        }
+    }
+}
---
+++ SearchFight/SearchBase.cs
@@ -0,0 +1,64 @@
+﻿using System;
+using System.Net;
+using System.IO;
+using System.Text;
+
+namespace SearchFight
+{
+    class SearchBase
+    {
+        public string Gadrress = "https://www.google.com/search?gl=us&hl=en&pws=0&q=";
+
+        public string Yadrress = "https://search.yahoo.com/search?p=";
+        public string Yadrress2 = "&fr=yfp-t&fp=1&toggle=1&cop=mss&ei=UTF-8";
+
+        public string Badrress = "https://www.bing.com/search?q=";
+
+        public String GoogleQuery(String query)
+        {
+            string uri = Gadrress + WebUtility.UrlEncode(query);
+            string tag = "<div id=\"resultStats\">";
+            int index;
+            int subex;
+            HttpWebRequest req = (HttpWebRequest)WebRequest.Create(uri);
+            HttpWebResponse resp = (HttpWebResponse)req.GetResponse();
+            StreamReader s = new StreamReader(resp.GetResponseStream(), Encoding.UTF8);
+            string result = s.ReadToEnd();
+            subex = result.IndexOf(tag);
+            index = subex + tag.Length;
+            result = result.Substring(index);
+            index = result.IndexOf("About ") + 6;
+            result = result.Substring(index);
+            index = result.IndexOf(" ");
+            result = result.Substring(0, index);
+
+            result = result.Replace(",", "");
+
+
+            return result;
+        }
+
+        public String BingQuery(String query)
+        {
+            string uri = Badrress + WebUtility.UrlEncode(query);
+            string tag = "<span class=\"sb_count\">";
+            int index;
+            int subex;
+            HttpWebRequest req = (HttpWebRequest)WebRequest.Create(uri);
+            HttpWebResponse resp = (HttpWebResponse)req.GetResponse();
+            StreamReader s = new StreamReader(resp.GetResponseStream(), Encoding.UTF8);
+            string result = s.ReadToEnd();
+            subex = result.IndexOf(tag);
+            index = subex + tag.Length;
+            result = result.Substring(index);
+            index = result.IndexOf(" ");
+            result = result.Substring(0, index);
+
+            result = result.Replace(".", "");
+
+
+            return result;
+        }
+
+    }
+}
